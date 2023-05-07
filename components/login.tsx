7a9a3079dfde02d02a0e4b93140a380ef388493f
LeBombusP <lebombusp@protonmail.com>
2023-05-07 03:31:48 +0200

    Working credentials login protorype

---
+++ components/login.tsx
@@ -0,0 +1,135 @@
+'use client';
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Separator } from '@/components/ui/separator';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import cookie from 'cookie';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+// import { redirect } from 'next/navigation';
+
+export default function Login() {
+  const router = useRouter();
+
+  const loginPasswordless = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get('email') as string;
+    const response = await fetch('/api/passwordless', {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+      }),
+    });
+    //inform user about email
+  };
+  const loginCredentials = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const username = formData.get('username') as string;
+    const password = formData.get('password') as string;
+    const response = await fetch('/api/credentials', {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+
+    const data = await response.json();
+    if (response.status === 200) {
+      cookie.serialize('jwt', data.jwt, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7,
+        secure: true,
+        httpOnly: true,
+      });
+
+      return router.push('/dashboard');
+      // return redirect('/dashboard');
+    }
+    return alert(data.error);
+  };
+  return (
+    <Tabs defaultValue='account' className='w-[400px] absolute top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2'>
+      <TabsList className='grid w-full grid-cols-2'>
+        <TabsTrigger value='account'>Username/Password</TabsTrigger>
+        <TabsTrigger value='password'>Email (passwordless)</TabsTrigger>
+      </TabsList>
+      <TabsContent value='account'>
+        <Card>
+          <form onSubmit={loginCredentials}>
+            <CardHeader>
+              <CardTitle className='m-auto'>Username/Password</CardTitle>
+              <CardDescription className='m-auto'>Log in here with username and password.</CardDescription>
+            </CardHeader>
+            <CardContent className='space-y-2'>
+              <div className='space-y-1'>
+                <Label htmlFor='username'>Username</Label>
+                <Input required name='username' placeholder='David Nowotny' />
+              </div>
+              <div className='space-y-1'>
+                <Label htmlFor='password'>Password</Label>
+                <Input required type='password' name='password' placeholder='••••••••' />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className='m-auto'>Log in with credentials</Button>
+            </CardFooter>
+          </form>
+        </Card>
+      </TabsContent>
+      <TabsContent value='password'>
+        <Card>
+          <form onSubmit={loginPasswordless}>
+            <CardHeader>
+              <CardTitle className='m-auto'>Email (passwordless)</CardTitle>
+              <CardDescription className='m-auto'>Submit your email and we will send you link to log in.</CardDescription>
+            </CardHeader>
+            <CardContent className='space-y-2'>
+              <div className='space-y-1'>
+                <Label htmlFor='email'>Email</Label>
+                <Input required type='email' name='email' placeholder='nowotny@mail.com' />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className='m-auto'>Log in via email</Button>
+            </CardFooter>
+          </form>
+        </Card>
+      </TabsContent>
+      <div className='w-full mt-2 inline-flex h-10 items-center justify-evenly rounded-md border-4 border-muted p-1 text-card-foreground'>
+        <AlertDialog>
+          <AlertDialogTrigger className='font-medium text-sm'>Forgot password?</AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Well sh*t!</AlertDialogTitle>
+              <AlertDialogDescription>
+                If you have forgotten your username or password you can still log id via email and change your username/password in the dashboard.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Sure</AlertDialogCancel>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+        <Separator orientation='vertical' />
+        <Link className='font-medium text-sm' href='/register'>
+          Dont have an account?
+        </Link>
+      </div>
+    </Tabs>
+  );
+}
--- pages/api/credentials.ts
+++ pages/api/credentials.ts
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
+import * as jose from 'jose';
+import 'dotenv/config';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { username, password } = req.body;
@@ -20,11 +22,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       },
     });
   } catch (error) {
-    if (error.message === 'NotFoundError') {
-      return res.status(401).json({ error: 'Wrong username or password' });
+    if (error.message == 'NotFoundError' || error.code == 'P2025') {
+      return res.status(401).json({ error: 'Invalid credentials' });
     }
     console.error(error);
     return res.status(500).json({ error: 'Server error' });
   }
-  return res.status(200);
+  const jwt = await new jose.SignJWT({})
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setSubject('urn:example:subject')
+    .setExpirationTime('2h')
+    .sign(new TextEncoder().encode(process.env.JWT_SECRET));
+
+  return res.status(200).json({ jwt });
 };
