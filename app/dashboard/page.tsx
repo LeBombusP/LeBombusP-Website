4217fbe8b12b1d9824025098093dd543999a32f8
LeBombusP <lebombusp@protonmail.com>
2023-05-14 17:51:01 +0200

    logout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,17 @@
+'use client'
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation'
+
 export default function Dashboard() {
+  const router = useRouter()
+  const logout = () => {
+    Cookies.remove('jwt', { path: '' })
+    router.push('/')
+  }
   return (
     <main>
       <p>Dashboard</p>
+      <button onClick={logout}>Logout</button>
     </main>
   );
 }
