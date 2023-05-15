fcb8d81623beca5898cde98a4ceccccfe8461898
LeBombusP <lebombusp@protonmail.com>
2023-05-15 22:41:21 +0200

    debug

--- middleware.ts
+++ middleware.ts
@@ -5,25 +5,35 @@ import { verifyJWT } from '@/lib/auth';
 export async function middleware(request: NextRequest) {
   //if dashbord and no jwt -> login
   if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    console.log("attemting to get to dashboard");
     let cookie = request.cookies.get('jwt')?.value;
     if (!cookie) {
+      console.log("no cookie (dashboard)");
       return NextResponse.rewrite(new URL('/login', request.url));
     }
     const auth = await verifyJWT(process.env.JWT_KEY,cookie);
     if (!auth?.verified) {
+      console.log("wrong jwt (dashboard)");
       return NextResponse.rewrite(new URL('/login', request.url));
     }
+    console.log("working (dashboard)");
+
   }
   //if login and jwt -> dashboard
-  // if (request.nextUrl.pathname.startsWith('/login')||request.nextUrl.pathname.startsWith('/signup')) {
-  //   let cookie = request.cookies.get('jwt')?.value;
-  //   if (cookie) {
-  //     const auth = await verifyJWT(process.env.JWT_KEY,cookie);
-  //     if (auth?.verified) {
-  //       return NextResponse.rewrite(new URL('/dashboard', request.url));
-  //     }
-  //   }
-  // }
+  if (request.nextUrl.pathname.startsWith('/login')||request.nextUrl.pathname.startsWith('/signup')) {
+    console.log("attemting to get to login");
+    let cookie = request.cookies.get('jwt')?.value;
+    if (cookie) {
+      console.log("got cookie (login)");
+      const auth = await verifyJWT(process.env.JWT_KEY,cookie);
+      if (auth?.verified) {
+        console.log("found jwt -> dashboard (login)");
+        return NextResponse.rewrite(new URL('/dashboard', request.url));
+      }
+      console.log("wrong jwt (login)");
+    }
+    console.log("no cookie (login)");
+  }
 
   return NextResponse.next()
 }
